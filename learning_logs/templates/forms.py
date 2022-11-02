33ffea6e2ef0247a312bc95f370db6f3ee0f9e79
Isah Jacob <Isahjacob0@gmail.com>
2022-11-02 10:27:56 +0100

    created the form file for the form page where users can enter submit information

---
+++ learning_logs/templates/forms.py
@@ -0,0 +1,10 @@
+from django.contrib.admin import forms
+
+from learning_logs.models import Topic
+
+
+class TopicForm(forms.ModelForm):
+    class Meta:
+        model = Topic
+        fields = ['text']
+        labels = {'text': ''}
